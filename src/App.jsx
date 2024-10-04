b3a0cfa542754ab05b6def105fc1400d0c2be82d
1ynxa <tktk.kanako925@gmail.com>
2024-10-04 22:08:47 +0900

    コンポーネント整理

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,12 @@ import { SecondaryButton } from "./components/atom/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import { HeaderOnly } from "./components/template/HeaderOnly";
-
-const user = {
-  name: "りょうへい",
-  image: "src/components/img/クラゲ.jpeg",
-  email: "aaaa@gmail.com",
-  phone: "000-0000-0000",
-  company: {
-    name: "テスト株式会社",
-  },
-  website: "http://a.com",
-};
+import { BrowserRouter } from "react-router-dom";
+import { DefaultLayout } from "./components/template/DefaultLayout";
+import { Router } from "./router/Router";
 
 function App() {
-  return (
-    <HeaderOnly>
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </HeaderOnly>
-  );
+  return <Router />;
 }
 
 export default App;
