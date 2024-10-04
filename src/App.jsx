a958b4828bbc8fded7960d147270f8c73b55f61b
1ynxa <tktk.kanako925@gmail.com>
2024-10-04 22:12:47 +0900

    不要なimport削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import { useState } from "react";
-import { PrimaryButton } from "./components/atom/button/PrimayButton";
-import { SecondaryButton } from "./components/atom/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/template/HeaderOnly";
-import { BrowserRouter } from "react-router-dom";
-import { DefaultLayout } from "./components/template/DefaultLayout";
 import { Router } from "./router/Router";
 
 function App() {
